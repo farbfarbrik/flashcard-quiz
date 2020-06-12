be82b3ae7256029fd78597639af77ec7a4853173
Sascha Dörnath <sascha.doernath@gmx.de>
2020-06-12 18:51:36 +0200

    Updates

---
+++ precache-manifest.c95aa93a473be67fbea1570f8bd59ddb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6b3ae4fcb314e18ea162f41b1222445",
+    "url": "/flashcard-quiz/index.html"
+  },
+  {
+    "revision": "836bb8abad3aa198b725",
+    "url": "/flashcard-quiz/static/css/main.a09c9037.chunk.css"
+  },
+  {
+    "revision": "40b3cf15673926c75901",
+    "url": "/flashcard-quiz/static/js/2.b103a0b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/flashcard-quiz/static/js/2.b103a0b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "836bb8abad3aa198b725",
+    "url": "/flashcard-quiz/static/js/main.52d417fc.chunk.js"
+  },
+  {
+    "revision": "93ea9f1a01135d8902e7",
+    "url": "/flashcard-quiz/static/js/runtime-main.a55b2756.js"
+  }
+]);
